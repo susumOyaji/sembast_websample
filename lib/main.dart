1c08cd077bcdc30be8ffd644e5b5ec7ed9b59934
susumOyaji <sumitomo0210@gmail.com>
2022-11-19 11:12:23 +0900

    up

--- lib/main.dart
+++ lib/main.dart
@@ -93,6 +93,39 @@ class MyAppBloc {
   }
 }
 
+
+
+class MyApp extends StatelessWidget {
+  final MyAppBloc bloc;
+
+  const MyApp({Key? key, required this.bloc}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sembast Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with 'flutter run'. You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // 'hot reload' (press 'r' in the console where you ran 'flutter run',
+        // or simply save your changes to 'hot reload' in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(
+        //title: 'Sembast Demo',
+        //bloc: bloc,
+      ),
+    );
+  }
+}
+
+
 class CakeApp extends StatefulWidget {
   const CakeApp({super.key});
   @override
